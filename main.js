594f81f426eb0083f831fc3e1609ce2e9b62ef68
VicDD1 <victor.duchesne-delapierre@etu.univ-smb.fr>
2025-11-16 12:53:06 +0100

    fin
    Final push

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var toggle_btn = document.querySelector('.theme-btn');
 var body = document.querySelector('body');
-var a = document.querySelectorAll('a');
+var clouds = document.querySelector('#clouds');
 var dark_theme_class = 'dark';
 var light_theme_class = 'light';
 
@@ -9,21 +9,15 @@ toggle_btn.addEventListener('click', function() {
     if (body.classList.contains(dark_theme_class)) {
         body.classList.remove(dark_theme_class);
         body.classList.add(light_theme_class);
+        clouds.classList.remove(dark_theme_class)
+        clouds.classList.add(light_theme_class)
         toggle_btn.innerHTML="Dark Theme";
     }   
     else if (body.classList.contains(light_theme_class)){
         body.classList.remove(light_theme_class);
         body.classList.add(dark_theme_class);
+        clouds.classList.remove(light_theme_class)
+        clouds.classList.add(dark_theme_class)
         toggle_btn.innerHTML="Light Theme";
     }
-    a.forEach((element) =>{ 
-        if (element.classList.contains(dark_theme_class)){
-            element.classList.remove(dark_theme_class);
-            element.classList.add(light_theme_class);
-        }
-        else if (element.classList.contains(light_theme_class)){
-            element.classList.remove(light_theme_class);
-            element.classList.add(dark_theme_class);
-        }
-    })
 });
